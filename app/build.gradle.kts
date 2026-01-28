80da13774d3bee9f942668f2c5da7ef7c1322e47
isaacrwamba019 <isaacrwamba019@gmail.com>
2026-01-28 20:55:12 +0000

    Fix build errors and resolve merge conflicts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.google.gms.google.services)
     alias(libs.plugins.hilt.android)
+    alias(libs.plugins.kotlin.compose)
     kotlin("kapt")
 }
 

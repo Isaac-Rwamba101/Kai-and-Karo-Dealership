b371057940bef829aa1fa0dfc85669d6d1467769
isaacrwamba019 <isaacrwamba019@gmail.com>
2026-02-03 19:47:39 +0000

    Fix: Resolve all build errors

---
+++ app/src/main/java/com/kaiandkaro/dealership/ui/screens/VehiclesScreen.kt
@@ -0,0 +1,9 @@
+package com.kaiandkaro.dealership.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun VehiclesScreen() {
+    Text(text = "Vehicles Screen")
+}
